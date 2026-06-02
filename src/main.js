667589f69304fe84b241ac42e21c53d8715446d9
Scott Loeb <sdloeb@signagesystems.com>
2026-06-02 00:07:09 -0400

    puttview5

--- src/main.js
+++ src/main.js
@@ -1246,8 +1246,8 @@ function animate() {
         const dirX = dX / len;
         const dirZ = dZ / len;
 
-        const rigidCamDist = 5.2;
-        const rigidCamHeight = 1.8;
+        const rigidCamDist = 7.0;
+        const rigidCamHeight = 1.0;
 
         cameraTargetPos.set(
             ball.position.x - dirX * rigidCamDist,
@@ -1257,9 +1257,9 @@ function animate() {
 
         // ABSOLUTE GAZE LOCK
         cameraLookAt.set(
-            ball.position.x + dirX * 5.2,
-            ball.position.y + 0.332,  // Change this line: Raised from 0.18 to 1.45 to tilt the camera up and force the ball down
-            ball.position.z + dirZ * 5.2
+            ball.position.x + dirX * 7.0,
+            ball.position.y + 0.55,  // Change this line: Raised from 0.18 to 1.45 to tilt the camera up and force the ball down
+            ball.position.z + dirZ * 7.0
         ); // Change this line
     }
 
@@ -1271,7 +1271,7 @@ function animate() {
     if (!physics.isMoving) {
         if (isCamOnGreen) {
             // Multiplies the target size by the inverse camera zoom ratio to keep its screen size perfectly normal
-            finalBallTargetScale *= 1.074; // Modify this line: Counteracts camera distance to keep ball size identical on screen
+            finalBallTargetScale *= 1.45; // Modify this line: Counteracts camera distance to keep ball size identical on screen
         }
     }
 
