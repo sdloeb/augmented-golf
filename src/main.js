e1cf9d2e75a678a134feb05a03aae2fe5c746632
Scott Loeb <sdloeb@gmail.com>
2026-06-11 09:47:32 -0400

    75 ball

--- src/main.js
+++ src/main.js
@@ -1432,7 +1432,7 @@ function animate() {
         } else {
             // Modify this block: Automatically scales down if screen width is mobile or portrait orientation
             const isMobile = window.innerWidth <= 768 || window.innerWidth / window.innerHeight < 1; // Add this line
-            ballTargetScale = isMobile ? 0.65 : 0.70; // Modify this line: 0.50 is themobile  size, 0.70 is the desktop size
+            ballTargetScale = isMobile ? 0.75 : 0.70; // Modify this line: 0.50 is themobile  size, 0.70 is the desktop size
         }
 
         const camDist = onGreen ? 2.5 : 7.5;
